4237ff96e22dba7827418d592fcefab3e20d0986
nameless <gfedcba625@gmail.com>
2018-10-04 23:24:45 +0300

    added removing of blog files, edit blog endpoint

--- chatServ/routes/blogs.js
+++ chatServ/routes/blogs.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const Blog = require('../models/blog');
+const fs = require('fs');
 
 const multer = require('multer');
 const storage = multer.diskStorage({
@@ -18,12 +19,12 @@ const upload = multer({
 });
 
 router.get('/getBlog/:userId', async(request, response) => {
-    let blogs = await Blog.find({owner: request.params.userId});
-    if (blogs) {
-        response.send(blogs);
-    } else {
-        response.sendStatus(404);
-    }
+    response.send(await Blog.find({owner: request.params.userId}));
+});
+
+router.get('/getPost/:postId', async(request, response) => {
+    response.send(await Blog.findOne({_id: request.params.postId}));
+
 });
 
 router.post('/addPost', upload.array('files', 12), async(request, response) => {
@@ -52,13 +53,52 @@ router.post('/addPost', upload.array('files', 12), async(request, response) => {
     } else {
         response.sendStatus(404);
     }
+});
 
+router.put('/editPost', upload.array('files', 12), async(request, response) => {
+    let post = {
+        attachedFiles: [],
+        textContent: request.body.content,
+        owner: request.user._id
+    };
+
+    if(request.body.existsFiles) {
+        post.attachedFiles.concat(request.body.existsFiles);
+    }
+
+    if(request.files.length > 0) {
+        request.files.forEach((file) => {
+            post.attachedFiles.push(`blogs/${file.filename}`);
+        });
+    }
+
+    if(!post.textContent) {
+        delete post.textContent;
+    }
+
+    if(!post.textContent && post.attachedFiles.length === 0) {
+        return response.sendStatus(404);
+    }
+
+    let newBlogPost = await Blog.findOneAndUpdate({
+        _id: request.body._id, owner: request.user._id}, post);
+    if(newBlogPost) {
+        response.send(newBlogPost);
+    } else {
+        response.sendStatus(404);
+    }
 });
 
 router.delete('/deletePost/:_id', async(request, response) => {
-    response.send(await Blog.findOneAndRemove({
+    let deletedItem = await Blog.findOneAndRemove({
         _id: request.params._id, owner: request.user._id
-    }));
+    });
+    if(deletedItem) {
+        deletedItem.attachedFiles.forEach(file => {
+            fs.unlink(`public/${file}`, err => console.log(err));
+        });
+    }
+    response.send(deletedItem);
 });
 
 module.exports = router;
