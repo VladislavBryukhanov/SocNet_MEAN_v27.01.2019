83d665aed8d59d3a8ad11c45166062c6294cb877
nameless <gfedcba625@gmail.com>
2018-10-25 10:58:07 +0300

    added rate for blog
    (cherry picked from commit 3d7f4e9)

--- src/app/models/rate.ts
+++ src/app/models/rate.ts
@@ -1,20 +1,13 @@
-export enum ItemsType {
-  post = 1,
-  photo = 2,
-  comment = 3
-}
-
 export class Rate {
+  public _id: string;
   public userId: string;
   public isPositive: boolean;
-  public itemType: number;
   public itemId: string;
   public lastState: boolean;
 
-  constructor(userId: string, isPositive: boolean, itemType: number, itemId: string) {
+  constructor(userId: string, isPositive: boolean, itemId: string) {
     this.userId = userId;
     this.isPositive = isPositive;
-    this.itemType = itemType;
     this.itemId = itemId;
   }
 }
