c6fc55a436d37bfe19ccb2c941f658139733934b
nameless <gfedcba625@gmail.com>
2018-10-26 19:31:14 +0300

    added ratedUsers endpoint

--- chatServ/routes/rate.js
+++ chatServ/routes/rate.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 const Rate = require('../models/rate');
+const bodyParser = require('body-parser');
 
 const actions = {
   LIKE: 'like',
   DISLIKE: 'dislike'
 };
+
+router.get('/getRatedUsers/:itemId&:isPositive&:count&:page', bodyParser.json(), async(request, response) => {
+    let itemId = request.params.itemId;
+    let isPositive = request.params.isPositive;
+    let maxCount = request.params.count;
+    let currentPage = request.params.page;
+
+    let usersId = await Rate.find(
+        {itemId, isPositive},
+        '-_id userId',
+        {skip: currentPage * maxCount, limit: maxCount});
+    usersId = usersId.map( item => item.userId);
+
+    let users = await User.find({_id: usersId});
+    response.send(users);
+});
 
 router.get('/getRateCounter/:itemId&:userId', async (request, response) => {
     let itemId = request.params.itemId;
@@ -15,7 +33,7 @@ router.get('/getRateCounter/:itemId&:userId', async (request, response) => {
     let dislikeCounter = await Rate.count({itemId, isPositive: false});
     let myAction = await Rate.findOne({itemId, userId, isPositive: true}) ? actions.LIKE
         : await Rate.findOne({itemId, userId, isPositive: false}) ? actions.DISLIKE : null;
-    
+
     response.send({likeCounter, dislikeCounter, myAction});
 });
 
